b4991802d36c68c990e6101dc943ed8339914485
Sara Bonnin <saesbori@gmail.com>
2023-09-08 11:55:16 +0100

    vars and operators

---
+++ src/operators/Runner.java
@@ -0,0 +1,60 @@
+package operators;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        /*OPERATORS
+        = assignment, assigns a value to a variable
+        + addition
+        - subtraction
+        * multiplies
+        / divide
+        % divide by and find remainder
+        += shortcut for adding and reassignment
+        Also works for all others: -=, *=, /=
+        ++ increments by 1
+        -- decrements by 1
+        */
+
+        int i = 12;  // assigns 12 to i
+        System.out.println(i + 2); // 14
+        System.out.println(i); // 12
+        System.out.println(i = i + 2);  // 14
+        System.out.println(i);  // 14
+
+        i = i + 3;  // Overrides i with a new value
+
+
+        System.out.println(12 + 12);
+        System.out.println(12 * 12);
+        System.out.println(12 - 10);
+        System.out.println(12 % 5);
+//        division of integers will return a truncated integer returning what's before the dot without rounding. A way around this  is to enter one of the 2 numbers to float by adding an F
+        System.out.println(12 / 7);
+        System.out.println(12 / 7F);
+
+        System.out.print("i:");
+        System.out.println(i);
+        System.out.print("i+=2:");
+        System.out.println(i+=2);
+        System.out.print("i++:");  // print THEN increments
+        System.out.println(i++);
+        System.out.print("++i:");  // increments THEN prints
+        System.out.println(++i);
+        System.out.print("i--:");
+        System.out.println(i--);
+
+//        Adding 2 strings it concatenates them (adds them together)
+        System.out.println("Hello " + "World");
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/variables/Runner.java
@@ -0,0 +1,35 @@
+package variables;
+
+public class Runner {
+    public static void main(String[] args) {
+//        Maximum values for each whole numbers (primitive data type):
+        byte b = 127;
+        short s = 246;
+        int i = 210_000_000;
+        long l = 999999999L; // note the L at the end to indicate is a long so JAVA knows is not an integer
+
+
+//        Decimal numbers (primitive data types)
+        float f = 589.56F;  // float is more efficient than double. We must add F at the end otherwise it thinks is a double
+        double d = 999999999.999999999;  // long float
+
+
+//        Booleans:
+        boolean bool = true;
+
+
+//        Text:
+        char c = 'j';
+//        We can use either literal or the new key. There are differences in terms of memory but this will be discussed later on
+        String stringLiteral = "Hello!"; // String - non-primitive, this is actually an object
+        String string = new String("I am an object");
+        String s1 = "\"Using scape characters\"";
+
+
+//        NAMING CONVENTION:
+//        variableNames are camelCase
+//        ClassNames are PascalCase
+
+
+    }
+}
