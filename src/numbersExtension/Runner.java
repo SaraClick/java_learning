2e5460d242a16ab1593c264cd5893a286ca88a1c
Sara Bonnin <saesbori@gmail.com>
2023-09-11 20:29:04 +0100

    add num extension, ex2

--- src/numbersExtension/Runner.java
+++ src/numbersExtension/Runner.java
@@ -1,5 +1,7 @@
 package numbersExtension;
 
+import java.lang.reflect.Array;
+
 public class Runner {
 
     // OPTION 1
@@ -28,15 +30,51 @@ public class Runner {
         return -1;
     }
 
+    public static String numToString(int num) {
+        if (num >= 1 && num <= 99) {
+            String result = "";
+            int dec = num / 10;
+            int unit = num % 10;
+
+            String[] units = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+            String[] decens = {"teen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+            if (num == 10) {
+                return "ten";
+            } else if (num == 11) {
+                return "eleven";
+            } else if (num == 12) {
+                return "twelve";
+            } else {
+                if (num <= 9) {
+                    return units[unit - 1];
+                } else {
+                    if (unit == 0) {
+                        return decens[dec - 1];
+                    } else {
+                        return decens[dec - 1] + "-" + units[unit - 1];
+                    }
+                }
+            }
+        }
+        return "Invalid input";
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(addNumbers(23));
-        System.out.println(addNumbers(76));
-        System.out.println(addNumbers(81));
+//        System.out.println(addNumbers(23));
+//        System.out.println(addNumbers(76));
+//        System.out.println(addNumbers(81));
+//
+//        System.out.println(addNumbers2(23));
+//        System.out.println(addNumbers2(76));
+//        System.out.println(addNumbers2(81));
 
-        System.out.println(addNumbers2(23));
-        System.out.println(addNumbers2(76));
-        System.out.println(addNumbers2(81));
+        System.out.println(numToString(25));
+        System.out.println(numToString(11));
+        System.out.println(numToString(6));
+        System.out.println(numToString(200));
+        System.out.println(numToString(30));
 
     }
 }
